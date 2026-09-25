5643bd69bc703c76d9363a749e306d1b3431b443
Eki Susanto <eq.susanto@gmail.com>
2026-09-25 23:57:38 +0700

    update kanaban style

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -32,45 +32,133 @@ function Home() {
                                     </div>
                                 </div>
                             </header>
-                            <div className="kanban-card">
-                                <div className="kanban-card-row">
-                                    <div className="kanban-card-row__img">
-                                        <img src="https://placehold.co/400x200" alt="" />
-                                    </div>
-                                    <div className="kanban-card-row__label">
-                                        <div className="label label--red">Bug</div>
-                                    </div>
-                                    <div className="kanban-card-row__progress">
-                                        <div className="kanban-card-row__progress-bar" style={{ width: "67%" }}></div>
-                                    </div>
-                                    <div className="kanban-card-row__title">
-                                        <p>Lorem ipsum dolor sit amet, adipisicing elit. Dolor quibusdam voluptates deleniti aperiam quidem temporibus nemo?</p>
-                                    </div>
-                                    <div className="kanban-card-row__bottom">
-                                        <div className="kanban-card-row__bottom-left self-center">
-                                            <ul>
-                                                <li>
-                                                    <div className="due-date warning">
-                                                        <Timer size={13} strokeWidth={2.5} />
-                                                        <span>18 Aug</span>
-                                                    </div>
-                                                </li>
-                                                <li className="sub-tasks self-center">
-                                                    <div>
-                                                        <SquareCheck size={13} strokeWidth={1.5} />
-                                                        <span>10/19</span>
-                                                    </div>
-                                                </li>
-                                                <li className="attachments self-center">
-                                                    <div>
-                                                        <Paperclip size={13} strokeWidth={1.4} style={{ transform: 'rotate(47deg)' }} />
-                                                        <span>2</span>
-                                                    </div>
-                                                </li>
-                                            </ul>
+                            <div className="kanban-card-wrapper">
+                                <div className="kanban-card">
+                                    <div className="kanban-card-row">
+                                        <div className="kanban-card-row__img">
+                                            <img src="https://placehold.co/400x200" alt="" />
                                         </div>
-                                        <div className="kanban-card-row__bottom-right">
-                                            <AvaStacked />
+                                        <div className="kanban-card-row__label">
+                                            <div className="label label--red">Bug</div>
+                                        </div>
+                                        <div className="kanban-card-row__progress">
+                                            <div className="kanban-card-row__progress-bar" style={{ width: "67%" }}></div>
+                                        </div>
+                                        <div className="kanban-card-row__title">
+                                            <p>Lorem ipsum dolor sit amet, adipisicing elit. Dolor quibusdam voluptates deleniti aperiam quidem temporibus nemo?</p>
+                                        </div>
+                                        <div className="kanban-card-row__bottom">
+                                            <div className="kanban-card-row__bottom-left self-center">
+                                                <ul>
+                                                    <li>
+                                                        <div className="due-date warning">
+                                                            <Timer size={13} strokeWidth={2.5} />
+                                                            <span>18 Aug</span>
+                                                        </div>
+                                                    </li>
+                                                    <li className="sub-tasks self-center">
+                                                        <div>
+                                                            <SquareCheck size={13} strokeWidth={1.5} />
+                                                            <span>10/19</span>
+                                                        </div>
+                                                    </li>
+                                                    <li className="attachments self-center">
+                                                        <div>
+                                                            <Paperclip size={13} strokeWidth={1.4} style={{ transform: 'rotate(47deg)' }} />
+                                                            <span>2</span>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                            <div className="kanban-card-row__bottom-right">
+                                                <AvaStacked />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="kanban-card">
+                                    <div className="kanban-card-row">
+                                        <div className="kanban-card-row__img">
+                                            <img src="https://placehold.co/400x200" alt="" />
+                                        </div>
+                                        <div className="kanban-card-row__label">
+                                            <div className="label label--red">Bug</div>
+                                        </div>
+                                        <div className="kanban-card-row__progress">
+                                            <div className="kanban-card-row__progress-bar" style={{ width: "67%" }}></div>
+                                        </div>
+                                        <div className="kanban-card-row__title">
+                                            <p>Lorem ipsum dolor sit amet, adipisicing elit. Dolor quibusdam voluptates deleniti aperiam quidem temporibus nemo?</p>
+                                        </div>
+                                        <div className="kanban-card-row__bottom">
+                                            <div className="kanban-card-row__bottom-left self-center">
+                                                <ul>
+                                                    <li>
+                                                        <div className="due-date warning">
+                                                            <Timer size={13} strokeWidth={2.5} />
+                                                            <span>18 Aug</span>
+                                                        </div>
+                                                    </li>
+                                                    <li className="sub-tasks self-center">
+                                                        <div>
+                                                            <SquareCheck size={13} strokeWidth={1.5} />
+                                                            <span>10/19</span>
+                                                        </div>
+                                                    </li>
+                                                    <li className="attachments self-center">
+                                                        <div>
+                                                            <Paperclip size={13} strokeWidth={1.4} style={{ transform: 'rotate(47deg)' }} />
+                                                            <span>2</span>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                            <div className="kanban-card-row__bottom-right">
+                                                <AvaStacked />
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="kanban-card">
+                                    <div className="kanban-card-row">
+                                        <div className="kanban-card-row__img">
+                                            <img src="https://placehold.co/400x200" alt="" />
+                                        </div>
+                                        <div className="kanban-card-row__label">
+                                            <div className="label label--red">Bug</div>
+                                        </div>
+                                        <div className="kanban-card-row__progress">
+                                            <div className="kanban-card-row__progress-bar" style={{ width: "67%" }}></div>
+                                        </div>
+                                        <div className="kanban-card-row__title">
+                                            <p>Lorem ipsum dolor sit amet, adipisicing elit. Dolor quibusdam voluptates deleniti aperiam quidem temporibus nemo?</p>
+                                        </div>
+                                        <div className="kanban-card-row__bottom">
+                                            <div className="kanban-card-row__bottom-left self-center">
+                                                <ul>
+                                                    <li>
+                                                        <div className="due-date warning">
+                                                            <Timer size={13} strokeWidth={2.5} />
+                                                            <span>18 Aug</span>
+                                                        </div>
+                                                    </li>
+                                                    <li className="sub-tasks self-center">
+                                                        <div>
+                                                            <SquareCheck size={13} strokeWidth={1.5} />
+                                                            <span>10/19</span>
+                                                        </div>
+                                                    </li>
+                                                    <li className="attachments self-center">
+                                                        <div>
+                                                            <Paperclip size={13} strokeWidth={1.4} style={{ transform: 'rotate(47deg)' }} />
+                                                            <span>2</span>
+                                                        </div>
+                                                    </li>
+                                                </ul>
+                                            </div>
+                                            <div className="kanban-card-row__bottom-right">
+                                                <AvaStacked />
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
