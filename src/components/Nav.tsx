46e0ae86b9388aff7b394d951bb425116ffdbb8b
Eki Susanto <eq.susanto@gmail.com>
2026-09-25 13:57:22 +0700

    update style nav

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,12 +1,12 @@
 import './Nav.css';
-import { LockKeyhole, ChevronDown, UserRoundPlus } from 'lucide-react';
+import { LockKeyhole, ChevronDown, UserRoundPlus, Funnel, RefreshCcwDot, Search } from 'lucide-react';
 
 function Nav() {
     return (
         <header className='container'>
             <nav>
-                <div>
-                    <button className="btn-transparent" type="button">
+                <div className="nav__left">
+                    <button className="btn-project-list" type="button">
                         <LockKeyhole size={20} />
                         <span className="text-project">Adhivasindo</span>
                         <ChevronDown size={20} />
@@ -19,9 +19,25 @@ function Nav() {
                         <div>2+</div>
                     </div>
                     <button className="btn-primary btn-primary__btn-group" type="button">
-                        <UserRoundPlus size={18} />
+                        <UserRoundPlus size={20} />
                         <span>Invite</span>
                     </button>
+                </div>
+                <div className="nav__right self-center">
+                    <button className="btn-transparent btn-transparent__btn-group" type="button">
+                        <Funnel size={20} />
+                        <span>Filter</span>
+                    </button>
+                    <button className="btn-transparent btn-transparent__btn-group" type="button">
+                        <RefreshCcwDot size={20} />
+                        <span>Export / Import</span>
+                    </button>
+                    <div className="input-group w-full">
+                        <button type='button'>
+                            <Search size={20} />
+                        </button>
+                        <input className="input w-full" type="text" placeholder='Search Tasks' />
+                    </div>
                 </div>
             </nav>
         </header>
