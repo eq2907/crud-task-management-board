125707d56233b369136c77db4ac3840acc7ba80c
Eki Susanto <eq.susanto@gmail.com>
2026-09-26 10:44:22 +0700

    create btn mark complete style

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,3 +1,5 @@
+import { X, Check, ImagePlus } from "lucide-react";
+
 function Modal() {
     return (
         <>
@@ -5,8 +7,26 @@ function Modal() {
                 <div className="modal">
                     <div className="modal-content">
                         <div className="modal-content__left">
-                            <div className="modal-content__header-left">
-
+                            <div className="modal-content__left-header">
+                                <div>
+                                    <label className="btn-complete">
+                                        <input type="checkbox" className="toggle-complete" />
+                                        <Check />
+                                        <span className="btn-text">Mark Complete</span>
+                                    </label>
+                                </div>
+                                <div>
+                                    <button className="btn-close btn btn--gray" type="button">
+                                        <X size={20} />
+                                    </button>
+                                </div>
+                            </div>
+                            <div className="modal-cover">
+                                <label className="btn-cover" htmlFor="modal-cover-input">
+                                    <ImagePlus size={50} />
+                                    <input type="file" id="modal-cover-input" />
+                                    <span className="btn-text">Add Cover Image</span>
+                                </label>
                             </div>
                         </div>
                         <div className="modal-content__right">
@@ -15,6 +35,7 @@ function Modal() {
                     </div>
                 </div>
             </div>
+            <div className="backdrop"></div>
         </>
     )
 }
